4521e89006ac474fda38684ad49b5b94c477667b
Niks Hariman <nhariman@hotmail.com>
2022-03-09 18:10:04 +0100

    have a working microshell to memorise

--- microshell.c
+++ microshell.c
@@ -6,7 +6,7 @@
 /*   By: nhariman <nhariman@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/03/04 15:55:57 by nhariman      #+#    #+#                 */
-/*   Updated: 2022/03/08 21:57:34 by nhariman      ########   odam.nl         */
+/*   Updated: 2022/03/09 18:09:32 by nhariman      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,10 @@ void	ft_print(int fd, char *str)
 	write(fd, str, i);
 }
 
-int		ft_execute(int i, char **av, char **envp)
+int		ft_execute(int i, char **av, char **envp, int tmp_fd)
 {
+	av[i] = NULL;
+	close(tmp_fd);
 	execve(av[0], av, envp);
 	ft_print(2, "error: cannot execute ");
 	ft_print(2, av[0]);
@@ -38,13 +40,14 @@ int		ft_execute(int i, char **av, char **envp)
 int		main(int argc, char **argv, char **envp)
 {
 	int fd[2];
-	int og_fd;
+	int tmp_fd;
+	int child_pid;
 	int i = 0;
 
 	if (argc == 1)
 		return 0;
-	og_fd = dup(0);
-	while (argv[i] != NULL)
+	tmp_fd = dup(STDIN_FILENO);
+	while (argv[i] && argv[i + 1])
 	{
 		argv = &argv[i + 1];
 		i = 0;
@@ -63,37 +66,55 @@ int		main(int argc, char **argv, char **envp)
 				ft_print(2, "\n");
 			}
 		}
-		else if (argv[i] && !strcmp(argv[i], "|"))
+		else if (argv != &argv[i] && (argv[i] == NULL || !strcmp(argv[i], ";"))) // HAS TO COME BEFORE | CHECK BC NULL
 		{
-			ft_print(1, "pipes here\n");
-		}
-		else
-		{
-			int child_pid;
-			int	child_stat;
-
 			child_pid = fork();
-			av[i] = NULL;
 			if (child_pid < 0)
 			{
 				ft_print(2, "error: fatal\n");
-				exit(1);
+				exit(-1);
 			}
 			else if (child_pid == 0)
-				ft_execute(i, argv, envp);
+			{
+				dup2(tmp_fd, STDIN_FILENO);
+				if (ft_execute(i, argv, envp, tmp_fd))
+					return (1);
+			}
 			else
 			{
+				close(tmp_fd);
 				waitpid(-1, NULL, WUNTRACED);
+				tmp_fd = dup(STDIN_FILENO);
 			}
-			ft_print(1, "semicolon or regular execute here\n");
 		}
-		// for(int j = 0; j < i; j++)
-		// {
-		// 	ft_print(1, argv[j]);
-		// 	ft_print(1, "\n");
-		// }
-		// ft_print(1,"end loop\n");
-
+		else if (argv != &argv[i] && !strcmp(argv[i], "|"))
+		{
+			pipe(fd); // pipe fd
+			child_pid = fork();
+			if (child_pid < 0)
+			{
+				ft_print(2, "error: fatal\n");
+				exit(-1);
+			}
+			else if (child_pid == 0)
+			{
+				dup2(tmp_fd, STDIN_FILENO);
+				dup2(fd[1], STDOUT_FILENO);
+				close(fd[0]);
+				close(fd[1]);
+				if (ft_execute(i, argv, envp, tmp_fd))
+					return (1);
+			}
+			else
+			{
+				close(fd[1]);
+				close(tmp_fd);
+				waitpid(-1, NULL, WUNTRACED);
+				tmp_fd = dup(fd[0]);
+				close(fd[0]);
+			}
+		}
 	}
+	close(tmp_fd);
 	return (0);
 }
